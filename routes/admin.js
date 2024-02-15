3426d84cbf8caf7f0806e69987e08d9bc92f40f2
suman <sumobarnwal@gmail.com>
2024-02-15 14:05:50 +0530

    controller

--- routes/admin.js
+++ routes/admin.js
@@ -6,15 +6,25 @@ const rootDir=require('../util/path');
 
 const router = express.Router();
 
+const products=[];
 
 router.get('/add-product', (req, res, next) => {
-   res.sendFile(path.join(rootDir,'views', 'add-product.html'))
+  //  res.sendFile(path.join(rootDir,'views', 'add-product.html'))
+  res.render('add-product',{
+    pageTitle: 'Add Product',
+    path: 'admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct:true
+  })
 });
 
 
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  //console.log(req.body);
+  products.push({title: req.body.title})
   res.redirect('/');
 });
 
 module.exports = router;
+exports.products=products;
